fbc80d178a870fbe9fa2d2f981d883512e785caa
AKadowski <463505@hull.ac.uk>
2016-11-05 03:25:00 +0000

    Enemy Spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public EnemyScript enemyInstance;
+	public GameObject defaultTarget;
+	public float msBetweenSpawns = 4000f;
+	float nextSpawnTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawnTime) 
+		{
+			nextSpawnTime = Time.time + msBetweenSpawns / 1000f;
+			EnemyScript enemy = Instantiate (enemyInstance, transform.position, transform.rotation) as EnemyScript;
+			enemy.SetTarget (defaultTarget);
+		} 
+	}
+}
