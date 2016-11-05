22e70b37809300eafce7272200faf22bc3486a4e
AKadowski <463505@hull.ac.uk>
2016-11-05 02:11:07 +0000

    Re-added bounce towers

---
+++ Assets/Scripts/bounceBombTower.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class bounceBombTower : MonoBehaviour
+{
+
+	GameObject currentTarget = null;
+	public float range = 10f;
+	public GameObject[] enemies;
+	public BounceBombProjectile projectile;
+	public float msBetweenShots = 500f;
+	float nextShotTime;
+	public float shotSpeed;
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Do we have a target?
+		// If not, try and find one
+		if (currentTarget == null)
+		{
+			float closestDistance = 0;
+			GameObject potentialTarget = null;
+			// Searches through all enemies and finds closest
+			enemies = GameObject.FindGameObjectsWithTag("Enemy");
+			foreach (GameObject enemy in enemies)
+			{
+				Vector3 distance = transform.position - enemy.transform.position;
+				if (distance.magnitude > closestDistance && distance.magnitude < range)
+					potentialTarget = enemy;
+			}
+
+			// If a enemy is found, make them our target
+			if (potentialTarget != null) 
+			{
+				currentTarget = potentialTarget;
+				nextShotTime = Time.time + msBetweenShots / 1000f;
+			}
+		}
+		else if (currentTarget != null)
+		{
+			Vector3 distance = transform.position - currentTarget.transform.position;
+			if (distance.magnitude > range)
+			{
+				currentTarget = null;
+			}
+			else
+			{
+				transform.LookAt(currentTarget.transform);
+				if (Time.time > nextShotTime)
+				{
+					nextShotTime = Time.time + msBetweenShots / 1000f;
+					BounceBombProjectile bullet = Instantiate(projectile, transform.forward * 1.1f + transform.position, transform.rotation) as BounceBombProjectile;
+					bullet.SetSpeed(shotSpeed);
+				}
+			}
+		}
+
+	}
+}
---
+++ Assets/Shooter/Scripts/BounceBombProjectile.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceBombProjectile : Projectile {
+
+	public ProjectileBouncing shrapnel;
+	public int amountOfShrapnel = 5;
+
+	void Start()
+	{
+		Destroy(gameObject, lifetime);
+
+		Collider[] initialCollisions = Physics.OverlapSphere(transform.position, .1f, collisionMask);
+		if (initialCollisions.Length > 0)
+		{
+			OnHitObject(initialCollisions[0], transform.position);
+		}
+
+		GetComponent<TrailRenderer>().material.SetColor("_Color", trailColour);
+	}
+
+	void Update()
+	{
+		float moveDistance = speed * Time.deltaTime;
+		CheckCollisions(moveDistance);
+		transform.Translate(Vector3.forward * moveDistance);
+	}
+
+	protected void CheckCollisions(float moveDistance)
+	{
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, moveDistance + skinWidth, collisionMask, QueryTriggerInteraction.Collide))
+		{
+			OnHitObject(hit.collider, hit.point);
+		}
+	}
+
+	virtual protected void OnHitObject(Collider c, Vector3 hitPoint)
+	{
+		IDamageable damageableObject = c.GetComponent<IDamageable>();
+		if (damageableObject != null)
+		{
+			damageableObject.TakeHit(damage, hitPoint, transform.forward);
+		}
+		GameObject.Destroy(gameObject);
+	}
+
+
+	void OnDestroy()
+	{
+		for (int i = 0; i < amountOfShrapnel; i++) 
+		{
+			ProjectileBouncing bouncy = Instantiate (shrapnel, transform.position, Random.rotation) as ProjectileBouncing;
+			bouncy.SetSpeed (10f);
+
+		}
+	}
+}
